e6e27116c09de07fb0f4f2455efc186f65aad8f4
Dan Johnson <jdjohnson8883@gmail.com>
2025-09-13 11:01:56 -0400

    Basic, by-the-book implementation of abstract `Employee` class

---
+++ src/main/java/edu/citadel/hw1/Employee.java
@@ -0,0 +1,31 @@
+
+import java.time.LocalDate;
+
+public abstract class Employee implements Comparable<Employee> {
+
+  private String name;
+  private LocalDate hireDate;
+
+  public Employee(String name, LocalDate hireDate) {
+    this.name = name;
+    this.hireDate = hireDate;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public LocalDate getHireDate() {
+    return this.hireDate;
+  }
+
+  public abstract double getMonthlyPay();
+
+  @Override
+  public int compareTo(Employee other) {
+    return Double.compare(
+        this.getMonthlyPay(),
+        other.getMonthlyPay());
+  }
+
+}
